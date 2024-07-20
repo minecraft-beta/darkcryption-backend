1599367f30481eaa53c7a40d6288cd17dc47765f
minecraft <149880559+minecraft-beta@users.noreply.github.com>
2024-07-20 18:23:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const fs = require('fs');
+const axios = require('axios');
 const app = express();
 const port = 3000;
+
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1261432791824990230/STXNnMXn9KzAgsjZ5J7kE4Dr0gIaUD-dJbOTE7EmVCaNLu1XmLXofUKlomMXkHfWZAtT';
 
 app.use(express.json());
 
@@ -16,15 +18,20 @@ app.post('/log-ip', (req, res) => {
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const logMessage = `Client IP: ${clientIp} - Date: ${new Date().toISOString()}\n`;
 
+  // Log the IP address to the console
   console.log(logMessage);
 
-  fs.appendFile('server.log', logMessage, (err) => {
-    if (err) {
-      console.error('Error writing to log file:', err);
-      res.status(500).send('Error logging IP address');
-      return;
-    }
-    res.status(200).send('IP address logged');
+    // Send the IP address to the Discord webhook
+    axios.post(DISCORD_WEBHOOK_URL, {
+      content: `New visitor IP logged: ${clientIp}`
+    })
+    .then(() => {
+      res.status(200).send('IP address logged and sent to Discord');
+    })
+    .catch((error) => {
+      console.error('Error sending to Discord webhook:', error);
+      res.status(500).send('Error logging IP address and sending to Discord');
+    });
   });
 });
 
